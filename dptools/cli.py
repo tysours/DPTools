ae55adc29b823c9e69feb6047b964f4c0910448d
Ty Sours <tsours@ucdavis.edu>
2022-07-01 09:19:41 -0700

    Add subparser functionality

--- dptools/cli.py
+++ dptools/cli.py
@@ -12,13 +12,15 @@ def main():
                                      description="DPTools CLI for doing stuff with deepmd-kit",
                                      )
     parser.add_argument("--version", action="version", version=__version__)
-    subparsers = parser.add_subparsers()
-    # TODO: Add logger
-    functions, parsers = {}, {}
+    subparsers = parser.add_subparsers(dest="command")
+    # TODO: Add logging
+    command_clis = {}
     for comm, mod in command2module.items():
         subparser = subparsers.add_parser(comm)
         CLI = import_module(mod).CLI
         cli = CLI(subparser)
-        cli.add()
+        cli.add_args()
+        command_clis[comm] = cli
 
     parsed_args = parser.parse_args()
+    command_clis[parsed_args.command].main(parsed_args)
