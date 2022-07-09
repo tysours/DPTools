5649d80acd44e93dbde722054d11c8725f497c29
Ty Sours <tsours@ucdavis.edu>
2022-07-09 14:45:56 -0700

    Initial

---
+++ dptools/lmp/calculator.py
@@ -0,0 +1,397 @@
+import numpy as np
+import lammps
+from dptools.lmp.lammps_io import LammpsInput
+from ase.calculators.calculator import (
+    Calculator, all_changes, PropertyNotImplementedError
+)
+from ase.units import GPa
+
+class LmpCalc(Calculator):
+    """
+    Base class for lammps ASE calculator
+    """
+
+    name = "Lammps"
+    implemented_properties = ["energy", "forces", "stress"]
+
+    def __init__(self, 
+                 type_map=None,
+                 minimize=False, 
+                 relax_UC=False,
+                 ftol=1e-3,
+                 lmp=None, 
+                 run_command=None, 
+                 verbose=False, 
+                 label="lamp", 
+                 **kwargs):
+
+        Calculator.__init__(self, label=label, **kwargs)
+
+        if lmp is None:
+            if verbose:
+                lmp = lammps.lammps()
+            else:
+                lmp = lammps.lammps(cmdargs="-screen none".split())
+
+        if type_map is not None:
+            self._type_map = type_map
+        if run_command is None:
+            run_command = ["run 0"]
+        elif isinstance(run_command, str):
+            run_command = [run_command]
+        if relax_UC:
+            minimize = True
+        self.lmp = lmp
+        self.minimize = minimize
+        self.relax_UC = relax_UC
+        self.ftol = ftol
+        self.run_command = run_command
+
+    def calculate(self, 
+                  atoms=None, 
+                  properties=["energy", "forces", "stress"],
+                  system_changes=all_changes):
+        """
+        Parameters
+        ----------
+        atoms : Optional[Atoms], optional
+            atoms object to run the calculation on, by default None
+        properties : List[str], optional
+            unused, only for function signature compatibility,
+        system_changes : List[str], optional
+            unused, only for function signature compatibility, by default all_changes
+        """
+        if atoms is not None:
+            self.atoms = atoms.copy()
+
+        # XXX: Is there a better way to do this? Always write input or no?
+        self.write_input(atoms)
+
+        if self.minimize:
+            self.lmp.command(f"minimize 0.0 {self.ftol} 10000 100000")
+        else:
+            for comm in self.run_command:
+                self.lmp.command(comm)
+
+        # lammps is annoying and jumbles up indices sometimes
+        ids = self.lmp.numpy.extract_atom("id").T[0]
+        self._sort = np.argsort(ids)
+
+        self.update_atoms(atoms)
+        energy = self.lmp.get_thermo("etotal")
+        forces = self.lmp.numpy.extract_atom("f")[self._sort]
+        stress = self.read_stress()
+        self.results["energy"] = energy
+        self.results["forces"] = forces
+        self.results["stress"] = stress
+
+    def update_atoms(self, atoms):
+        positions = self.lmp.numpy.extract_atom("x")[self._sort]
+        # shift positions to align with cell at origin
+        shift = np.array([self.lmp.get_thermo(l) for l in ["xlo", "ylo", "zlo"]])
+        atoms.positions = positions - shift
+        atoms.cell = self.read_cell()
+
+    def read_stress(self):
+        pxx = self.lmp.get_thermo("pxx") * 1e-3 # kBar
+        pyy = self.lmp.get_thermo("pyy") * 1e-3
+        pzz = self.lmp.get_thermo("pzz") * 1e-3
+        pxy = self.lmp.get_thermo("pxy") * 1e-3
+        pyz = self.lmp.get_thermo("pyz") * 1e-3
+        pxz = self.lmp.get_thermo("pxz") * 1e-3
+        stress = -np.array([pxx, pyy, pzz, pyz, pxz, pxy]) * 1e-1 * GPa
+        return stress
+
+    def read_cell(self):
+        a = self.lmp.get_thermo("cella")
+        b = self.lmp.get_thermo("cellb")
+        c = self.lmp.get_thermo("cellc")
+        alpha = self.lmp.get_thermo("cellalpha")
+        beta = self.lmp.get_thermo("cellbeta")
+        gamma = self.lmp.get_thermo("cellgamma")
+        return [a, b, c, alpha, beta, gamma]
+
+    def write_input(self):
+        """System / forcefield specific"""
+        pass
+
+
+class DeepMD(LmpCalc):
+    name = "DP"
+    implemented_properties = ["energy", "forces", "stress"]
+    def __init__(self, 
+                 graph,
+                 type_map=None,
+                 minimize=False, 
+                 relax_UC=False,
+                 ftol=1e-3,
+                 lmp=None, 
+                 run_command=None, 
+                 verbose=False, 
+                 label="lamp", 
+                 **kwargs):
+
+        super().__init__(
+                       type_map=type_map,
+                       minimize=minimize, 
+                       relax_UC=relax_UC,
+                       ftol=ftol,
+                       lmp=lmp, 
+                       run_command=run_command, 
+                       verbose=verbose, 
+                       label=label, 
+                       **kwargs
+                       )
+
+        self.style = f'deepmd {graph}'
+
+    def set_atoms(self, atoms=None):
+        if not hasattr(self, '_type_map'):
+            self.set_types(atoms)
+        if 0 in atoms.get_tags():
+            for a in atoms:
+                a.tag = self._type_map[a.symbol]
+
+    def set_charges(self):
+        self.charges = np.zeros(len(self.atoms))
+
+    def set_types(self, atoms=None):
+        if atoms is None:
+            atoms = self.atoms
+        symbols = np.unique(atoms.get_chemical_symbols())
+        types = {s: i + 1 for i, s in enumerate(symbols)}
+        for a in atoms:
+            a.tag = types[a.symbol]
+        self._type_map = types
+        self.types = {v: k for k, v in types.items()} # inverting dict, probably stupid
+
+    def set_coeffs(self):
+        self.coeffs = None
+        self.pair_coeffs = None
+
+    def write_input(self, atoms=None):
+        # XXX maybe add this to base class if identical for other FFs too
+        if atoms is not None:
+            self.atoms = atoms
+        self.set_atoms(atoms)
+        self.set_charges()
+        self.set_coeffs()
+        self.io = LammpsInput(self.atoms, 
+                              self.types, 
+                              charges=self.charges,
+                              pair_style=self.style,
+                              coeffs=self.coeffs,
+                              kspace_style=None,
+                              pair_coeffs=self.pair_coeffs)
+
+        self.io.write_atoms(self.atoms)
+        self.io.write_input()
+        self.lmp.command("clear")
+        self.lmp.file("in.atoms")
+        if self.relax_UC:
+            self.lmp.command("fix 1 all box/relax tri 1.0")
+
+
+class ClayFF(LmpCalc):
+    """class for the classical ClayFF. Unused for deepmd but I will leave it here for future reference"""
+    name = "ClayFF"
+    implemented_properties = ["energy", "forces", "stress"]
+    def __init__(self, 
+                 type_map=None,
+                 minimize=False, 
+                 relax_UC=False,
+                 ftol=1e-3,
+                 lmp=None, 
+                 run_command=None, 
+                 verbose=False, 
+                 label="lamp", 
+                 cutoff=12.0,
+                 **kwargs):
+
+        super().__init__(
+                       type_map=type_map,
+                       minimize=minimize, 
+                       relax_UC=relax_UC,
+                       ftol=ftol,
+                       lmp=lmp, 
+                       run_command=run_command, 
+                       verbose=verbose, 
+                       label=label, 
+                       **kwargs
+                       )
+
+        self.rc = cutoff
+        self.style = f'lj/cut/coul/long {self.rc}'
+
+    def set_atoms(self, atoms=None):
+        if not hasattr(self, '_type_map'):
+            self.set_types(atoms)
+        if 0 in atoms.get_tags():
+            for a in atoms:
+                a.tag = self._type_map[a.symbol]
+        #lengths = atoms.cell.lengths()
+        #repeat = [int((self.rc * 2) // l + 1) for l in lengths]
+        #if hasattr(self, 'repeat'):
+        #    if repeat != self.repeat:
+        #        raise Exception("REPEAT CHANGED, FIX IT OR FIX LOSS FCN")
+        #self.repeat = repeat 
+        #atoms = atoms.repeat(self.repeat)
+        #self.atoms = atoms
+
+    def set_charges(self):
+        q_vals = {'Si': 2.1, 'O': -1.05}
+        charges = [q_vals[a.symbol] for a in self.atoms]
+        self.charges = np.array(charges)
+
+    def set_types(self, atoms=None):
+        if atoms is None:
+            atoms = self.atoms
+        symbols = np.unique(atoms.get_chemical_symbols())
+        types = {s: i + 1 for i, s in enumerate(symbols)}
+        for a in atoms:
+            a.tag = types[a.symbol]
+        self._type_map = types
+        self.types = {v: k for k, v in types.items()} # inverting dict, probably stupid
+
+    def set_coeffs(self):
+        epsilon = {'Si': 7.981163385703463e-08, 'O': 0.006738781799175867}
+        sigma = {'Si': 3.302027, 'O': 3.165541}
+        self.coeffs = {k: {'eps': epsilon[v], 'sig': sigma[v]}
+                        for k, v in self.types.items()}
+
+    def write_input(self, atoms=None):
+        if atoms is not None:
+            self.atoms = atoms
+        self._check_cell()
+        self.set_atoms(atoms)
+        self.set_charges()
+        self.set_coeffs()
+        self.io = LammpsInput(self.atoms, 
+                              self.types, 
+                              charges=self.charges,
+                              pair_style=self.style,
+                              coeffs=self.coeffs,
+                              cutoff=self.rc)
+
+        self.io.write_atoms(self.atoms)
+        self.io.write_input()
+        self.lmp.command('clear')
+        self.lmp.file('in.atoms')
+        if self.relax_UC:
+            self.lmp.command('fix 1 all box/relax tri 1.0')
+
+    def _check_cell(self):
+        '''Checks if lattice constants are > 2 * cutoff'''
+        min_lc = min(self.atoms.cell.lengths())
+        if min_lc < 2 * self.rc:
+            raise Exception("Lattice constant < 2 * cutoff\nRepeat Atoms or decrease cutoff")
+        return
+
+
+class BKS(LmpCalc):
+    """class for the classical BKS force field. Unused for deepmd but I will leave it here for future reference"""
+    #raise 
+    name = "BKS"
+    implemented_properties = ["energy", "forces", "stress"]
+    def __init__(self, 
+                 type_map=None,
+                 minimize=False, 
+                 relax_UC=False,
+                 ftol=1e-3,
+                 lmp=None, 
+                 run_command=None, 
+                 verbose=False, 
+                 label="lamp", 
+                 cutoff=12.0,
+                 **kwargs):
+
+        super().__init__(
+                       type_map=type_map,
+                       minimize=minimize, 
+                       relax_UC=relax_UC,
+                       ftol=ftol,
+                       lmp=lmp, 
+                       run_command=run_command, 
+                       verbose=verbose, 
+                       label=label, 
+                       **kwargs
+                       )
+
+        self.rc = cutoff
+        self.style = f'buck/coul/long {self.rc}'
+
+    def set_atoms(self, atoms=None):
+        if not hasattr(self, '_type_map'):
+            self.set_types(atoms)
+        if 0 in atoms.get_tags():
+            for a in atoms:
+                a.tag = self._type_map[a.symbol]
+        #lengths = atoms.cell.lengths()
+        #repeat = [int((self.rc * 2) // l + 1) for l in lengths]
+        #if hasattr(self, 'repeat'):
+        #    if repeat != self.repeat:
+        #        raise Exception("REPEAT CHANGED, FIX IT OR FIX LOSS FCN")
+        #self.repeat = repeat 
+        #atoms = atoms.repeat(self.repeat)
+        #self.atoms = atoms
+
+    def set_charges(self):
+        q_vals = {'Si': 2.4, 'O': -1.2}
+        charges = [q_vals[a.symbol] for a in self.atoms]
+        self.charges = np.array(charges)
+
+    def set_types(self, atoms=None):
+        if atoms is None:
+            atoms = self.atoms
+        symbols = np.unique(atoms.get_chemical_symbols())
+        types = {s: i + 1 for i, s in enumerate(symbols)}
+        for a in atoms:
+            a.tag = types[a.symbol]
+        self._type_map = types
+        self.types = {v: k for k, v in types.items()} # inverting dict, probably stupid
+
+    def set_coeffs(self):
+        self.coeffs = None
+
+        Aij = {'Si-O': 18003.7572, 'O-O': 1388.7730, 'Si-Si': 0.0}
+        bij = {'Si-O': 1 / 4.87318, 'O-O': 1 / 2.760, 'Si-Si': 1.0}
+        cij = {'Si-O': 133.5381, 'O-O': 175.000, 'Si-Si': 0.0}
+
+        pair_coeffs = ""
+        for k in Aij:
+            ij = [self._type_map[k.split('-')[0]], self._type_map[k.split('-')[1]]]
+            if ij[0] > ij[1]:
+                ij.reverse()
+            pair_coeffs += f"pair_coeff {ij[0]} {ij[1]} {Aij[k]} {bij[k]} {cij[k]}\n"
+
+        self.pair_coeffs = pair_coeffs
+
+    def write_input(self, atoms=None):
+        # XXX maybe add this to base class if identical for other FFs too
+        if atoms is not None:
+            self.atoms = atoms
+        self._check_cell()
+        self.set_atoms(atoms)
+        self.set_charges()
+        self.set_coeffs()
+        self.io = LammpsInput(self.atoms, 
+                              self.types, 
+                              charges=self.charges,
+                              pair_style=self.style,
+                              coeffs=self.coeffs,
+                              pair_coeffs=self.pair_coeffs,
+                              cutoff=self.rc)
+
+        self.io.write_atoms(self.atoms)
+        self.io.write_input()
+        self.lmp.command('clear')
+        self.lmp.file('in.atoms')
+        if self.relax_UC:
+            self.lmp.command('fix 1 all box/relax tri 1.0')
+
+    def _check_cell(self):
+        '''Checks if lattice constants are > 2 * cutoff'''
+        min_lc = min(self.atoms.cell.lengths())
+        if min_lc < 2 * self.rc:
+            raise Exception("Lattice constant < 2 * cutoff\nRepeat Atoms or decrease cutoff")
+        return
---
+++ dptools/lmp/lammps_io.py
@@ -0,0 +1,411 @@
+#!/usr/bin/env python
+#import lammps
+import numpy as np
+import os
+import sys
+import json
+from ase.neighborlist import NeighborList
+from ase.neighborlist import natural_cutoffs
+from ase.data import atomic_masses, atomic_numbers
+from ase.geometry.analysis import Analysis
+from textwrap import dedent
+from ase.calculators.lammpslib import convert_cell
+
+#with open(f'uff4mof.json', 'r') as file:
+#    lj_types = json.load(file)
+
+class LammpsInput:
+    """
+    Creates lammps input files from ASE Atoms object
+    Messy and needs to be redone
+
+    Parameters
+    ----------
+    atoms: ASE Atoms object, structure used for lammps calculation
+    types: dict, map atom type index to element symbol
+        e.g., {1: 'O', 2: 'Si'}
+    charges: list, list of charges (float) on each atom 
+    write_bonds: bool, define all bonds of structure in structure data file
+    name: str, name used for creating input files
+    atom_style: str, lammps style for atomic positions, groups, etc
+    pair_style: str, lammps pair potential style command
+    bond_style: str, lammps bond style command
+    angle_style: str, lammps angle style command
+    kspace_style: str, lammps kspace style command
+    groups: list, list of groups for each atom
+    coeffs: dict, maps FF params to atom types, #TODO: rework this
+    pair_coeffs: list[str], pair_coeff lines to define ij interactions
+    cutoff: float, cutoff radius for relevant pair potentials
+    """
+        
+    def __init__(self, atoms, types, charges=None, 
+                 write_bonds=False,
+                 name='atoms',
+                 atom_style='full',
+                 pair_style='lj/cut/coul/long',
+                 bond_style=None,
+                 angle_style=None,
+                 kspace_style='pppm 1.0e-5',
+                 groups=None,
+                 coeffs=None,
+                 pair_coeffs=None,
+                 cutoff=9.0, 
+                 sf=None):
+
+        if charges is None:
+            charges = atoms.get_initial_charges()
+
+        if groups is None:
+            groups = np.zeros(len(atoms), dtype=int)
+
+        if sf is None:
+            sf = [1, 1]
+
+        self.atoms = atoms
+        self.types = types
+        self.coeffs = coeffs
+        self.pair_coeffs = pair_coeffs
+        self.charges = charges
+        self.groups = groups
+        self.atom_style = atom_style
+        self.pair_style = pair_style
+        self.angle_style = angle_style
+        self.bond_style = bond_style
+        self.kspace_style = kspace_style
+        self.write_bonds = write_bonds
+        self.cutoff = cutoff
+        self.sf = sf
+        self.n = len(atoms)
+        self.symbols = np.array(atoms.get_chemical_symbols())
+        self.name = name
+
+    def set_atoms(self, atoms):
+        self.atoms = atoms
+
+    def write_atoms(self, atoms=None, charges=None):
+        if atoms is not None:
+            self.set_atoms(atoms)
+        if charges is not None:
+            self.charges = charges
+        self._write_atoms(self.write_bonds, self.atom_style)
+
+    def write_input(self):
+        self.input = self._get_input(self.atom_style, 
+                                     self.pair_style, 
+                                     self.bond_style, 
+                                     self.angle_style, 
+                                     self.kspace_style, 
+                                     self.groups, 
+                                     self.cutoff)
+
+
+    def _write_atoms(self, write_bonds=False, atom_style='full'):
+        n_atoms = len(self.atoms)
+        n_types = len(self.types.keys())
+
+        types_str = ""
+        coeffs_str = ""
+        if self.coeffs is not None:
+            coeffs_str += "Pair Coeffs\n\n"
+        for k, v in self.types.items():
+            sym = v.split('_')[0]
+            mass = atomic_masses[atomic_numbers[sym]]
+            types_str += f"{k} {mass} # {v}\n"
+            if self.coeffs is not None:
+                eps = self.coeffs[k]['eps'] * self.sf[0] 
+                sig = self.coeffs[k]['sig'] * self.sf[1]
+                coeffs_str += f"{k} {eps} {sig}\n"
+
+        #### POSITIONS ####
+        coords_str = ""
+
+        for a, g, q, (x, y, z) in zip(self.atoms, 
+                                      self.groups,
+                                      self.charges, 
+                                      self.atoms.positions):
+
+            #coords_str += f" {a.index+id_start} {g} {a.tag} {q} {x:.5f} {y:.5f} {z:.5f}\n"
+            if atom_style == 'full':
+                coords_str += f" {a.index + 1} {g} {a.tag} {q} {x} {y} {z}\n"
+            else:
+                coords_str += f" {a.index + 1} {a.tag} {x} {y} {z}\n"
+
+
+        #### BONDS ####
+        if write_bonds:
+            bonds_str = self.get_bonds()
+            n_bonds = self.n_bonds
+            n_b_types = self.n_b_types
+        else:
+            bonds_str = ""
+            n_bonds = 0
+            n_b_types = 0
+
+        temp = DataTemplate(self.name)
+        temp.natoms = n_atoms
+        temp.nbonds = n_bonds
+        temp.ntypes = n_types
+        temp.nbtypes = n_b_types
+        temp.cell = self.atoms.cell
+        temp.types = types_str
+        temp.atom_style = atom_style
+        temp.coeffs = coeffs_str
+        temp.coords = coords_str
+        temp.bonds = bonds_str
+
+        temp.write(f"data.{self.name}", temp.data)
+
+        return 
+
+            
+    def _get_input(self, 
+                   atom_style, 
+                   pair_style, 
+                   bond_style, 
+                   angle_style, 
+                   kspace_style, 
+                   groups, 
+                   cutoff):
+
+        if cutoff is None:
+            cutoff = ''
+
+        temp = InputTemplate(self.name)
+        temp.atom_style = atom_style 
+        temp.pair_style = pair_style
+        temp.bond_style = bond_style
+        temp.angle_style = angle_style
+        if self.coeffs is None and self.pair_coeffs is None:
+            temp.data += "pair_coeff * *\n"
+        if self.pair_coeffs is not None:
+            if isinstance(self.pair_coeffs, str):
+                temp.data += self.pair_coeffs
+            elif isinstance(self.pair_coeffs, list):
+                for line in self.pair_coeffs:
+                    temp.data += line
+            else:
+                raise TypeError("str or list needed for pair_coeffs")
+        if kspace_style is not None:
+            temp.data += f"kspace_style\t{kspace_style}\n\n"
+        if groups is not None:
+            #print("REMINDER TO REMOVE GROUP HARDCODING")
+            temp.data += "group\tsilica molecule 0\n"
+#            for n, group in groups.items():
+#                temp.data += f"group\t{group} molecule {n}\n"
+        temp.write(f"in.{self.name}", temp.data)
+        return temp.data
+
+    def get_coords(self):
+        pass
+
+    def get_bonds(self):
+        anal = Analysis(self.atoms)
+        bond_types = {}
+
+        def b_str(t1, t2): return f"{t1}-{t2}" if t1 < t2 else f"{t2}-{t1}"
+        for i, bonds in enumerate(anal.unique_bonds[0]):
+            for bond in bonds:
+                b_type = b_str(self.tags[i], self.tags[bond])
+                if b_type in bond_types:
+                    bond_types[b_type].append((i, bond))
+                else:
+                    bond_types[b_type] = [(i, bond)]
+            
+        self.n_b_types = len(bond_types.keys())
+        k = 1
+
+        bonds_str = "Bonds\n\n"
+        n_bond = 0
+        for i, (b_type, bonds) in enumerate(bond_types.items()):
+            for bond in bonds:
+                bonds_str += f"\t{n_bond+1}\t{i+k}\t{bond[0]+1}\t{bond[1]+1}\n"
+                n_bond += 1
+
+        self.n_bonds = n_bond
+        return bonds_str
+
+class Template:
+    def write(self, name, text):
+        with open(name, 'w') as file:
+            file.write(text)
+
+        
+class DataTemplate(Template):
+    def __init__(self, name):
+        self.name = name
+        self.data = dedent('''\
+        LAMMPS data file
+
+          <natoms> atoms
+          <nbonds> bonds
+          <ntypes> atom types
+          <nbtypes> bond types
+          0 angle types
+
+          <x> xlo xhi
+          <y> ylo yhi
+          <z> zlo zhi
+          <xyxzyz> xy xz yz
+
+        Masses
+
+        <types>
+
+        <coeffs>
+
+        Atoms # <atom_style>
+
+        <coords>
+
+        <bonds>
+        ''')
+
+    @property
+    def natoms(self):
+        return
+
+    @natoms.setter
+    def natoms(self, value):
+        self.data = self.data.replace("<natoms>", str(value))
+
+    @property
+    def nbonds(self):
+        return
+
+    @natoms.setter
+    def nbonds(self, value):
+        self.data = self.data.replace("<nbonds>", str(value))
+
+    @property
+    def ntypes(self):
+        return
+
+    @ntypes.setter
+    def ntypes(self, value):
+        self.data = self.data.replace("<ntypes>", str(value))
+
+    @property
+    def nbtypes(self):
+        return
+
+    @nbtypes.setter
+    def nbtypes(self, value):
+        self.data = self.data.replace("<nbtypes>", str(value))
+
+    @property
+    def cell(self):
+        return
+
+    @cell.setter
+    def cell(self, atoms_cell):
+
+        cell = convert_cell(atoms_cell)[0]
+        x = f"0 {cell[0, 0]}"
+        y = f"0 {cell[1, 1]}"
+        z = f"0 {cell[2, 2]}"
+        xy, xz, yz = cell[0, 1], cell[0, 2], cell[1, 2]
+        xyxzyz = f"{xy} {xz} {yz}"
+
+        self.data = self.data.replace("<x>", x)
+        self.data = self.data.replace("<y>", y)
+        self.data = self.data.replace("<z>", z)
+        self.data = self.data.replace("<xyxzyz>", xyxzyz)
+
+    @property
+    def types(self):
+        return
+
+    @types.setter
+    def types(self, value):
+        self.data = self.data.replace("<types>", str(value))
+
+    @property
+    def atom_style(self):
+        return
+
+    @atom_style.setter
+    def atom_style(self, value):
+        self.data = self.data.replace("<atom_style>", str(value))
+
+    @property
+    def coeffs(self):
+        return
+
+    @coeffs.setter
+    def coeffs(self, value):
+        self.data = self.data.replace("<coeffs>", str(value))
+
+    @property
+    def coords(self):
+        return
+
+    @coords.setter
+    def coords(self, value):
+        self.data = self.data.replace("<coords>", str(value))
+
+    @property
+    def bonds(self):
+        return
+
+    @coords.setter
+    def bonds(self, value):
+        self.data = self.data.replace("<bonds>", str(value))
+
+class InputTemplate(Template):
+    def __init__(self, name):
+        self.name = name
+        self.data = dedent(f'''\
+        units\t\tmetal
+        boundary\tp p p
+        atom_style\t<atom_style>
+        atom_modify\tmap array
+
+        pair_style\t<pair_style>
+        <bond_style>
+        <angle_style>
+
+        box tilt\tlarge
+        read_data\tdata.{name}
+        thermo_style custom step temp etotal pe press pxx pyy pzz pxy pxz pyz lx ly lz vol
+        ''')
+
+    @property
+    def atom_style(self):
+        return
+
+    @atom_style.setter
+    def atom_style(self, value):
+        self.data = self.data.replace("<atom_style>", str(value))
+
+    @property
+    def pair_style(self):
+        return
+
+    @pair_style.setter
+    def pair_style(self, value):
+        self.data = self.data.replace("<pair_style>", str(value))
+
+    @property
+    def bond_style(self):
+        return
+
+    @bond_style.setter
+    def bond_style(self, value):
+        if value is None:
+            self.data = self.data.replace("<bond_style>", "")
+        else:
+            self.data = self.data.replace("<bond_style>",
+                                          f"bond_style\t{value}")
+
+    @property
+    def angle_style(self):
+        return
+
+    @angle_style.setter
+    def angle_style(self, value):
+        if value is None:
+            self.data = self.data.replace("<angle_style>", "")
+        else:
+            self.data = self.data.replace("<angle_style>",
+                                          f"angle_style\t{value}")
+
