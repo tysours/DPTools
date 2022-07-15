0ae44597f7d9cbc1c63c6c1656c3feffc9110c64
Ty Sours <tsours@ucdavis.edu>
2022-07-15 07:36:43 -0700

    Update dependencies

--- setup.py
+++ setup.py
@@ -1,32 +1,32 @@
 from dptools import __version__
+from setuptools import setup, find_packages
 
 install_requires = [
-    'numpy',
-    'matplotlib',
-    'seaborn'
+    'ase',
+    'scikit-learn', # only needed for shuffle util, maybe write own function to avoid dependency
 ]
 
 
 python_requires = ">=3.6"
+package_data = {} # maybe useful someday
+#packages = find_packages(where='dptools', include=['*.py'],  # alternatively: `exclude=['additional*']`
+#        ) # TODO: Figure out how to use this
+
+packages = ['dptools', 'dptools.lmp']
 
 setup(name='dptools',
-      version=__version__,
+      version=f"{__version__}.test",
       description='CLI toolkit for working with deepmd-kit',
       url='https://github.com/tysours/DPTools',
       maintainer='Ty Sours',
       maintainer_email='tsours@ucdavis.edu',
       license='MIT',
-      packages=find_packages(),
+      packages=packages,
       python_requires=python_requires,
       install_requires=install_requires,
-      extras_require=extras_require,
+      #extras_require=extras_require,
       package_data=package_data,
-      entry_points={'console_scripts': ['ase=ase.cli.main:main',
-                                        'ase-db=ase.cli.main:old',
-                                        'ase-gui=ase.cli.main:old',
-                                        'ase-run=ase.cli.main:old',
-                                        'ase-info=ase.cli.main:old',
-                                        'ase-build=ase.cli.main:old']},
+      entry_points={'console_scripts': ['dptools=dptools.cli:main']},
       classifiers=[
           'Development Status :: 2 - Pre-Alpha',
           'License :: OSI Approved :: MIT License',
@@ -34,4 +34,3 @@ setup(name='dptools',
           'Programming Language :: Python :: 3',
           'Intended Audience :: Science/Research',
           'Topic :: Scientific/Engineering :: Chemistry'])
-
