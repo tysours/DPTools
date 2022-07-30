aa2107e5466f82b0926eae04f7eb023ac55ad706
Ty Sours <tsours@ucdavis.edu>
2022-07-29 17:18:23 -0700

    Add sbatch defaults for submitting jobs

--- dptools/env.py
+++ dptools/env.py
@@ -1,17 +1,59 @@
 import os
+import socket
 import dotenv
 from dptools.cli import BaseCLI
 from dptools.utils import typemap2str, str2typemap, read_type_map, graph2typemap
 
-def set_env(key, value):
+basedir = os.path.abspath(os.path.dirname(__file__))
+default_env_file = os.path.join(basedir, ".env")
+
+
+def set_env(key, value, env=None):
+    env_file = env if env else default_env_file
+
     dotenv.set_key(env_file, key, value)
 
-def get_dpfaults():
+
+def get_env(env=None):
+    env_file = env if env else default_env_file
+
+    values = dotenv.dotenv_values(env_file)
+    return values
+
+
+def get_dpfaults(key="model", env=None):
     """ like defaults but for dp (haha... ha..) """
-    defaults = dotenv.dotenv_values(env_file)
-    graph = defaults.get("DPTOOLS_MODEL", "./graph.pb")
-    type_map = defaults.get("DPTOOLS_TYPE_MAP", None)
-    return graph, type_map
+    default_vals = get_env(env)
+
+    if key == "model":
+        keys = ["DPTOOLS_MODEL", "DPTOOLS_TYPE_MAP"]
+    elif key == "sbatch":
+        keys = ["SBATCH_COMMENT", 
+                "OMP_NUM_THREADS", 
+                "TF_INTRA_OP_PARALLELISM_THREADS", 
+                "TF_INTER_OP_PARALLELISM_THREADS"]
+        if not default_vals.get(keys[0], None):
+            from dptools.hpc import hpc_defaults
+            host = socket.gethostname()
+            try:
+                for k, v in hpc_defaults[host].items():
+                    set_env(k, str(v), env=env)
+            except KeyError:
+                raise Exception("Host unrecognized and no default HPC parameters found."\
+                    "\nUse 'dptools set script.sh' with desired #SBATCH comment in script.sh")
+                    # XXX: What kind of exception would this be?
+
+            default_vals = get_env(env)
+            print("WARNING: set new default HPC parameters to env")
+            print("\nSettings:")
+            print("-" * 64)
+            for k in keys:
+                print(k, "=", default_vals[k])
+            print("-" * 64)
+            
+    defaults = tuple([default_vals.get(k) for k in keys])
+    return defaults
+
 
 def set_model(model):
     graph = os.path.abspath(model)
@@ -20,14 +62,14 @@ def set_model(model):
     set_env("DPTOOLS_MODEL", graph)
     set_env("DPTOOLS_TYPE_MAP", type_map_str)
 
+
 def set_sbatch(script):
     raise NotImplementedError("Harass me for this if you need it")
+
 
 def set_params(params):
     raise NotImplementedError("Harass me for this if you need it")
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-env_file = os.path.join(basedir, ".env")
 
 class CLI(BaseCLI):
     def add_args(self):
