f1bc4c0c7e0852847ec946c4041720ab07ecb32b
Ty Sours <tsours@ucdavis.edu>
2022-07-30 07:01:33 -0700

    Add slurm job submission

--- dptools/input.py
+++ dptools/input.py
@@ -11,6 +11,7 @@ import requests
 
 from dptools.cli import BaseCLI
 from dptools.utils import randomize_seed
+from dptools.hpc import SlurmJob
 
 class DeepInput:
     def __init__(self, db_name, atoms=None, system_name=None, type_map=None, n=None, path="./data"):
@@ -200,6 +201,8 @@ class CLI(BaseCLI):
         self.parser.add_argument("dbs", nargs='+', metavar="db", help="ASE .db files")
         self.parser.add_argument("-e", "--ensemble", action="store_true",
                 help="Make ensemble (4) of DP models to train")
+        self.parser.add_argument("-s", "--submit", action="store_true",
+                help="Automatically submit job(s) to train model(s) once input has been created")
         self.parser.add_argument("-n", nargs=1, type=int,
                 help="Max number of images to take from each db")
         self.parser.add_argument("-p", "--path", nargs=1, type=str, default="./data",
@@ -210,15 +213,20 @@ class CLI(BaseCLI):
             raise NotImplementedError("n needs to be reworked, sorry (harass me if you need it)")
         sys_names = [db.split("/")[-1].split(".db")[0] for db in args.dbs]
         thing = DeepInputs(args.dbs, system_names=sys_names, path=args.path)
-        if args.ensemble:
-            self.make_ensemble()
+        self.ensemble = args.ensemble
+        if self.ensemble:
+            self.make_ensemble() # sets self.dirs
+        else:
+            self.dirs = ["."]
+        if args.submit:
+            self.submit_jobs()
 
     def make_ensemble(self):
         with open("in.json") as file:
             in_json = json.loads(file.read())
         in_jsons = [randomize_seed(in_json) for _ in range(4)]
-        dirs = ["00", "01", "02", "03"]
-        for jsn, d in zip(in_jsons, dirs):
+        self.dirs = ["00", "01", "02", "03"]
+        for jsn, d in zip(in_jsons, self.dirs):
             self.write_json(jsn, d)
 
     @staticmethod
@@ -227,3 +235,15 @@ class CLI(BaseCLI):
         file_path = os.path.join(dest, "in.json")
         with open(file_path, "w") as file:
             file.write(json.dumps(src, indent=4))
+
+    def submit_jobs(self):
+        from dptools.env import get_dpfaults
+        hpc_info = get_dpfaults(key="sbatch")
+        sbatch_comment = hpc_info.pop("SBATCH_COMMENT")
+        commands = ["dp train in.json", "dp freeze -o graph.pb"]
+        jobs = SlurmJob(sbatch_comment,
+                        commands=commands,
+                        directories=self.dirs,
+                        file_name="dptools.train.sh",
+                        **hpc_info)
+        jobs.submit()
