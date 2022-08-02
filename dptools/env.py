bb06bc6a142a1d000bd725d19327b1e2273faa6a
Ty Sours <tsours@ucdavis.edu>
2022-08-02 12:11:15 -0700

    Update set command to accept model ensembles

--- dptools/env.py
+++ dptools/env.py
@@ -62,12 +62,13 @@ def get_dpfaults(key="model"):
     return defaults
 
 
-def set_model(model):
+def set_model(model, n_model=""):
     graph = os.path.abspath(model)
-    type_map = graph2typemap(graph)
-    type_map_str = typemap2str(type_map)
-    set_env("DPTOOLS_MODEL", graph)
-    set_env("DPTOOLS_TYPE_MAP", type_map_str)
+    set_env(f"DPTOOLS_MODEL{n_model}", graph)
+    if not n_model: # only write type_map once if setting ensemble of models
+        type_map = graph2typemap(graph)
+        type_map_str = typemap2str(type_map)
+        set_env(f"DPTOOLS_TYPE_MAP", type_map_str)
 
 
 def set_sbatch(script):
@@ -84,17 +85,21 @@ class CLI(BaseCLI): # XXX: Everything about this could surely be improved
              "Need .pb, .yaml, or .sh extension to set model, params, or sbatch, respectively."
         self.parser.add_argument(
             "thing",
-            nargs=1,
+            nargs="+",
             help=help
         )
 
     def main(self, args):
-        self.set(args.thing[0])
+        for i, t in enumerate(args.thing):
+            if t.endswith(".pb") and i > 0:
+                self.set(t, n_model=i+1)
+            else:
+                self.set(t)
 
-    def set(self, thing):
+    def set(self, thing, **kwargs):
         ext2function = {"pb": set_model, "sh": set_sbatch, "yaml": set_params}
         ext = thing.split(".")[-1]
         if ext not in ext2function:
             raise TypeError(f"Unrecognized file type for {thing}. Try 'dptools set -h'")
         self.set_thing = ext2function[ext]
-        self.set_thing(thing)
+        self.set_thing(thing, **kwargs)
