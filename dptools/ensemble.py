8c98b6175f8cb32bf69a1c80ce50510de9abda4d
Ty Sours <tsours@ucdavis.edu>
2022-08-03 20:16:10 +0000

    Add CLI for 'sample' command

--- dptools/ensemble.py
+++ dptools/ensemble.py
@@ -1,8 +1,9 @@
-from ase.io import read
+from ase.io import read, write
 import numpy as np
 import random
+import os
 
-from dptools.utils import graph2typemap
+from dptools.utils import graph2typemap, read_type_map
 from dptools.cli import BaseCLI
 
 class SampleConfigs:
@@ -17,6 +18,8 @@ class SampleConfigs:
         self.graphs = graphs
 
     def get_dev(self):
+        from deepmd.infer import calc_model_devi
+        from deepmd.infer import DeepPot as DP
         pos = np.array([a.get_positions().flatten() for a in self.configs])
         cell = np.array([a.cell.array.flatten() for a in self.configs])
         types = [self.type_map[a.symbol] for a in self.configs[0]]
@@ -28,14 +31,11 @@ class SampleConfigs:
 
     def sample(self, lo=0.05, hi=0.35, n=300):
         dev = self.get_dev()
-        i_configs = np.where(np.logcal_and(dev>=lo, dev<=hi))
+        i_configs = np.where(np.logical_and(dev>=lo, dev<=hi))[0]
         n_sample = n if n < len(i_configs) else len(i_configs)
-        i_new_configs = random.sample(i_configs, n_sample)
+        i_new_configs = random.sample(list(i_configs), n_sample)
         new_configs = [self.configs[i] for i in i_new_configs]
         return new_configs
-
-
-#def get_dev(atoms, graphs, type_map):
 
 
 class CLI(BaseCLI):
@@ -45,6 +45,34 @@ class CLI(BaseCLI):
             "configurations",
             help=help
         )
+        self.parser.add_argument("-n", type=int, default=300,
+                help="Max number of new configurations to select")
+        self.parser.add_argument("--lo", type=float, default=0.05,
+                help="Min value of eps_t (force dev) to select new configs from")
+        self.parser.add_argument("--hi", type=float, default=0.35,
+                help="Max value of eps_t (force dev) to select new configs from")
+        self.parser.add_argument("-o", "--output", nargs=1, type=str, default="new_configs.traj",
+                help="File to write new configurations to")
+        self.parser.add_argument("-m", "--model-ensemble", nargs="+", type=str,
+                help="Paths to ensemble of models or label of set models")
+
 
     def main(self, args):
-        raise NotImplementedError("Coming soon...")
+        configs = read(args.configurations, index=":")
+        outfile = os.path.abspath(args.output)
+        self.load_ensemble(args.model_ensemble) # sets self.type_map and self.graphs
+
+        sampler = SampleConfigs(configs, self.ensemble, read_type_map(self.type_map))
+        new_configs = sampler.sample(lo=args.lo, hi=args.hi, n=args.n)
+        write(outfile, new_configs)
+
+    def load_ensemble(self, ensemble):
+        if not ensemble:
+            from dptools.env import get_dpfaults
+            defaults = get_dpfaults(key="ensemble")
+            self.type_map, *ensemble = defaults
+        elif len(ensemble) == 1:
+            raise NotImplementedError("ensemble label not implemented, harass me if you need it")
+        else:
+            self.type_map = graph2typemap(ensemble[0])
+        self.ensemble = ensemble
