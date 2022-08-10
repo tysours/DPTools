2ad9c1085d55edb1f2702383ce5d77e8f058bfd5
Ty Sours <tsours@ucdavis.edu>
2022-08-10 14:53:30 -0700

    Implement variable .sh script commands for multiple job submissions

--- dptools/hpc.py
+++ dptools/hpc.py
@@ -29,13 +29,15 @@ class SlurmJob:
                  commands="",
                  directories=".",
                  file_name="script.sh",
+                 zip_commands=False,
                  **kwargs
                  ):
 
         self.sbatch = sbatch_comment
+        self._zip = zip_commands
         self.commands = commands
         self.set_path_stuff(directories, file_name) # what's the term for this? setting directories + file_name + full path
-        self.set_text(**kwargs)
+        self.set_sh_text(**kwargs)
 
     def get_header(self):
         header = f"#!/usr/bin/env bash\n{self.sbatch}\n"
@@ -48,7 +50,7 @@ class SlurmJob:
         self.file_name = file_name
         self.paths = [os.path.join(d, file_name) for d in self.directories]
 
-    def set_text(self, **kwargs):
+    def set_sh_text(self, **kwargs):
         header = self.get_header()
 
         exports = ""
@@ -56,13 +58,34 @@ class SlurmJob:
             exports += f"export {k}={v}\n"
 
         body = "\n"
-        if isinstance(self.commands, list):
+        if not self._zip and isinstance(self.commands, list):
             for comm in self.commands:
                 body += comm + "\n"
         elif isinstance(self.commands, str):
             body += self.commands
 
+        else:    # do not write commands to body if self._zip
+            pass # in this case, self.commands contains
+                 # list of unique commands for each submission dir
+
         self.text = header + exports + body
+
+    @property
+    def text(self):
+        if self._zip:
+            return self._text + next(self.comm_generator)
+        else:
+            return self._text
+
+    @text.setter
+    def text(self, new_text):
+        self._text = new_text
+        if self._zip:
+            self.comm_generator = self.generate_commands()
+
+    def generate_commands(self):
+        for comm in self.commands:
+            yield comm
 
     def _write_file(self, path):
         with open(path, "w") as file:
