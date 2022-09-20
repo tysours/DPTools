f1302581ab7ae1f720c70be8fad510c325b0ca29
Ty Sours <tsours@ucdavis.edu>
2022-09-20 07:58:16 -0700

    Rework default json loading and type_map pathing

--- dptools/cli/train.py
+++ dptools/cli/train.py
@@ -8,7 +8,21 @@ from dptools.hpc import SlurmJob
 
 
 class CLI(BaseCLI):
+    """
+    Setup and submit jobs to train single or ensemble of deepmd-kit models.
+
+    Complete documentation: https://dptools.rtfd.io/en/latest/commands/train.html
+
+    Examples:
+        dptools train /path/to/dataset # simple single model
+        dptools train -e /path/to/dataset # ensemble (4) of models
+        dptools train -e -s /path/to/dataset # submit 4 slurm jobs to train ensemble
+        dptools train -p /path/to/training/dir /path/to/dataset # specify dir to train in
+        dptools train -i /path/to/in.json /path/to/dataset # specify in.json parameter file
+    """
+
     help_info = "Setup and submit jobs to train deepmd-kit models"
+
     def add_args(self):
         self.parser.add_argument("dataset", type=str,
                 help="Path to dataset parent directory")
@@ -18,6 +32,8 @@ class CLI(BaseCLI):
                 help="Automatically submit slurm job(s) to train model(s)")
         self.parser.add_argument("-p", "--path", type=str, default=".",
                 help="Specify path to training directory")
+        self.parser.add_argument("-i", "--input", type=str, default=None,
+                help="Specify path to in.json deepmd parameter file to use for training")
 
     def main(self, args):
         if args.dataset == ".":
@@ -25,21 +41,28 @@ class CLI(BaseCLI):
                     " (dataset can not be '.')")
         self.datapath = os.path.abspath(args.dataset)
         self.path = os.path.abspath(args.path)
-        if args.ensemble:
-            self.make_ensemble() # sets self.dirs
+        if args.input:
+            self._json = os.path.abspath(args.input)
         else:
-            self.dirs = [path]
+            default = os.path.dirname(os.path.abspath(__file__))
+            self._json = os.path.join(default, "../train/in.json")
+
+        if args.ensemble:
+            ens_dirs = ["00", "01", "02", "03"]
+            self.dirs = [os.path.join(self.path, d) for d in ens_dirs]
+        else:
+            self.dirs = [self.path]
         self._sub = args.submit
+        self.setup()
         self.submit_jobs()
 
-    def make_ensemble(self):
-        with open("in.json") as file:
+    def setup(self):
+        with open(self._json, "r") as file:
             in_json = json.loads(file.read())
-        ens_dirs = ["00", "01", "02", "03"]
-        self.dirs = [os.path.join(self.path, d) for d in ens_dirs]
         for d in self.dirs:
             jsn = randomize_seed(in_json)
-            jsn = self.link_dirs(in_json)
+            jsn = self.link_dirs(jsn)
+            jsn = self.set_types(jsn)
             self.write_json(jsn, d)
 
     @staticmethod
@@ -56,6 +79,16 @@ class CLI(BaseCLI):
         validation = [os.path.join(d, "validation") for d in dirs]
         in_json["training"]["training_data"]["systems"] = train
         in_json["training"]["validation_data"]["systems"] = validation
+        return in_json
+
+    def set_types(self, in_json):
+        if not hasattr(self, "types"): # only need to read type_map once
+            type_map_path = os.path.join(self.datapath, "type_map.json")
+            with open(type_map_path, "r") as file:
+                type_map = json.loads(file.read())
+            self.types = [type_map[str(i)] for i in range(len(type_map))]
+
+        in_json["model"]["type_map"] = self.types
         return in_json
 
     @staticmethod
