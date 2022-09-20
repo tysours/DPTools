7b1fc51159503b2bd86cc0ab8da3210cdb002b27
Ty Sours <tsours@ucdavis.edu>
2022-09-20 05:24:51 -0700

    Make new argparse formatter to retain docstring formatting

--- dptools/cli/__init__.py
+++ dptools/cli/__init__.py
@@ -1,6 +1,8 @@
-from dptools import __version__
-from importlib import import_module
 import argparse
+from importlib import import_module
+from textwrap import dedent, fill
+
+from dptools import __version__
 
 commands = (
     "input",
@@ -16,23 +18,39 @@ commands = (
 )
 
 class BaseCLI:
+    """
+    Base class for CLI commands. More or less just a template for reference.
+
+    Args:
+        parser (argparse.ArgumentParser): argparse parser for parsing CLI command arguments
+    """
     help_info = ""
+
     def __init__(self, parser):
         self.parser = parser
 
     def add_args(self):
         '''Command specific arguments'''
-        pass
+        return
 
     def main(self, args):
         '''Command specific main method'''
-        pass
+        return
+
+
+class MyFormatter(argparse.ArgumentDefaultsHelpFormatter):
+    """Simple HelpFormatter that dedents text but retains docstring format"""
+
+    def _fill_text(self, text, width, indent):
+        return dedent(text)
 
 
 def main():
     parser = argparse.ArgumentParser(prog="dptools",
-                                     description="DPTools CLI for doing stuff with deepmd-kit",
-                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+                                     description="DPTools CLI for doing stuff with deepmd-kit\n\n"\
+                                             "Complete documentation available at: "\
+                                             "https://dptools.rtfd.io",
+                                     formatter_class=MyFormatter,
                                      )
     parser.add_argument("--version", action="version", version=__version__)
     subparsers = parser.add_subparsers(dest="command")
@@ -41,8 +59,9 @@ def main():
     for comm in commands:
         mod = "dptools.cli." + comm
         CLI = import_module(mod).CLI
-        subparser = subparsers.add_parser(comm, help=CLI.help_info,
-                formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        doc = CLI.__doc__
+        subparser = subparsers.add_parser(comm, help=CLI.help_info, description=doc,
+                formatter_class=MyFormatter)
         cli = CLI(subparser)
         cli.add_args()
         command_clis[comm] = cli
