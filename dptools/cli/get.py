c13875939bcc23f1273e90578059a373efbedc40
Ty Sours <tsours@ucdavis.edu>
2022-09-22 15:51:08 -0300

    Add in.json to gettable things

--- dptools/cli/get.py
+++ dptools/cli/get.py
@@ -1,9 +1,13 @@
+import os
+import shutil
+
 from dptools.cli import BaseCLI
 from dptools.simulate.parameters import get_parameter_sets, write_yaml
 
 class CLI(BaseCLI):
     """
-    Get params.yaml for specific simulation type.
+    Get params.yaml for specific simulation type. Can also be used to get
+    in.json file (deepmd-kit training parameters).
 
     :doc:`Complete documentation here<../commands/get>`
 
@@ -14,12 +18,12 @@ class CLI(BaseCLI):
         $ dptools get cellopt
         $ dptools get nvt-md
         $ dptools get nvt-md.900K # custom simulation params
+        $ dptools get in.json # get training param file
     """
     help_info = "Get params.yaml for specific calculation type"
     def add_args(self):
         self.parser.add_argument(
             "calculation",
-            nargs=1,
             type=str,
             help="Calculation type to generate params.yaml file "
             "(spe, opt, cellopt, nvt-md, npt-md, eos, vib)."
@@ -27,8 +31,13 @@ class CLI(BaseCLI):
         )
 
     def main(self, args):
+        if args.calculation.endswith(".json"):
+            here = os.path.dirname(__file__)
+            json_path = os.path.abspath(os.path.join(here, "../train/in.json"))
+            shutil.copy(json_path, ".")
+            return
+
         param_sets = get_parameter_sets()
-        params = param_sets[args.calculation[0]]
+        params = param_sets[args.calculation]
         with open("params.yaml", "w") as file:
             write_yaml(params, file)
-        return
