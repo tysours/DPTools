7b9b4d63d04ef20b487e8a7228f7d38bcb00ff92
Ty Sours <tsours@ucdavis.edu>
2022-09-22 12:47:12 -0700

    Add extra test command

--- tests/input/test.sh
+++ tests/input/test.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-#dptools input dft_inputs/0?_???.db
+dptools input dft_inputs/0?_???.db
 #dptools input dft_inputs/0*.traj
+#dptools input dft_inputs/0* # should fail
 #dptools input -n 200 dft_inputs/0*.traj
-dptools input -p path_test dft_inputs/0*.traj
+#dptools input -p path_test dft_inputs/0*.traj
