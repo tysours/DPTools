0b2eb25051b8521d61cac9edd728dc38eff92480
Ty Sours <tsours@ucdavis.edu>
2022-09-22 12:41:01 -0700

    Create input command tests

---
+++ tests/input/test.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+#dptools input dft_inputs/0?_???.db
+#dptools input dft_inputs/0*.traj
+#dptools input -n 200 dft_inputs/0*.traj
+dptools input -p path_test dft_inputs/0*.traj
