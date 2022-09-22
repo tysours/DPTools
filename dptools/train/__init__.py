0e6f81e0a865784e53efa5dbb41e4a94f36a37b4
Ty Sours <tsours@ucdavis.edu>
2022-09-22 15:04:57 -0300

    Create __init__ for sphinx-apidoc recognition

---
+++ dptools/train/__init__.py
@@ -0,0 +1,5 @@
+from dptools.train.ensemble import SampleConfigs
+from dptools.train.input import DeepInput, DeepInputs
+from dptools.train.parity import EvaluateDP
+
+__all__ = ["DeepInput", "DeepInputs", "EvaluateDP", "SampleConfigs"]
